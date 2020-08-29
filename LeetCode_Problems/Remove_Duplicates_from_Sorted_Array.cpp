50e7600bd86ff87018b01e517535259c32b626ad
Guru Sarath <gurusarath1.official@gmail.com>
2020-08-29 16:01:30 -0500

    Better way to remove duplicates
    Better way to remove duplicates

--- LeetCode_Problems/Remove_Duplicates_from_Sorted_Array.cpp
+++ LeetCode_Problems/Remove_Duplicates_from_Sorted_Array.cpp
@@ -3,6 +3,23 @@ Given a sorted array nums, remove the duplicates in-place such that each element
 Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
 */
 
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        for(unsigned int i = 1; i < nums.size(); i++)
+        {
+            if(nums[i] == nums[i-1])
+            {
+                nums.erase(nums.begin() + i);
+                i = i - 1;
+            }
+        }
+        return nums.size();   
+    }
+};
+
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
