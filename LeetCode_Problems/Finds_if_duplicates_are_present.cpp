834c061c3f266864af672788681f15e203f62682
Guru Sarath <gurusarath1.official@gmail.com>
2020-08-30 16:11:15 -0500

    Create Finds_if_duplicates_are_present.cpp

---
+++ LeetCode_Problems/Finds_if_duplicates_are_present.cpp
@@ -0,0 +1,19 @@
+// Sort the array
+// and check if the array contains duplicates
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        for(unsigned int i=1; i<nums.size(); i++)
+        {
+            if(nums[i] == nums[i-1])
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
