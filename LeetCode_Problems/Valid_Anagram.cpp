ab4d5d7c3bc136d085e7607156f255d2e628a7ae
Guru Sarath <gurusarath1.official@gmail.com>
2020-09-01 19:17:50 -0500

    Update Valid_Anagram.cpp

--- LeetCode_Problems/Valid_Anagram.cpp
+++ LeetCode_Problems/Valid_Anagram.cpp
@@ -18,6 +18,10 @@ What if the inputs contain unicode characters? How would you adapt your solution
 */
 
 
+
+// Method 1
+// Sort both the strings and check if they are equal 
+
 class Solution {
 public:
     bool isAnagram(string s, string t) {
@@ -31,6 +35,35 @@ public:
         sort(t.begin(), t.end());
         
         if(s == t)
+        {
+            return true;
+        } else {
+            return false;
+        }   
+    }
+};
+
+// Method 2
+// Use a Hash map and count the number of number of times each character repeats
+// Check if both the maps are same
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size())
+        {
+            return false;
+        }
+        
+        map<char, int> s_char_count;
+        map<char, int> t_char_count;
+        for(int i=0; i<s.size(); i++)
+        {
+            s_char_count[s[i]] += 1;
+            t_char_count[t[i]] += 1;
+        }
+        
+        if(s_char_count == t_char_count)
         {
             return true;
         } else {
