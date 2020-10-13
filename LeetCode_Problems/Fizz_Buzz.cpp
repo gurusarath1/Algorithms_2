c4f4188939d60c6254e909769a62449813da2f23
Guru Sarath <gurusarath1.official@gmail.com>
2020-10-13 02:14:46 -0500

    Create Fizz_Buzz.cpp

---
+++ LeetCode_Problems/Fizz_Buzz.cpp
@@ -0,0 +1,35 @@
+/*
+Write a program that outputs the string representation of numbers from 1 to n.
+
+But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+*/
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        
+        vector<string> output;
+        
+        for(int i=1; i<=n; i++) {
+            
+            if(i%3 == 0 && i%5==0) {
+                output.push_back("FizzBuzz");
+                continue;
+            }
+            
+            if(i%3 == 0) {
+                output.push_back("Fizz");
+                continue;
+            }
+            
+            if(i%5 == 0) {
+                output.push_back("Buzz");
+                continue;
+            }
+            
+            output.push_back(to_string(i));   
+        }
+        
+        return output;
+        
+    }
+};
