51ddfe2295b23164c537e9aa886c5483c2f4528b
Guru Sarath <gurusarath1.official@gmail.com>
2020-12-14 13:59:06 -0600

    Top down

--- Dynamic_Programming/Pattern_1/0_1_Knapsack.cpp
+++ Dynamic_Programming/Pattern_1/0_1_Knapsack.cpp
@@ -1,3 +1,9 @@
+/*
+Given two integer arrays to represent weights and profits of ‘N’ items, 
+we need to find a subset of these items which will give us maximum profit such that their cumulative weight is not more than a given number ‘C’. 
+Each item can only be selected once, which means either we put an item in the knapsack or skip it.
+*/
+
 #include <iostream>
 #include <vector>
 #include <map>
