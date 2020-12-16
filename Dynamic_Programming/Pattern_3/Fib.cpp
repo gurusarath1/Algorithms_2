e9ecad72c892baf2b15d6933214fff11e3be6c77
Guru Sarath <gurusarath1.official@gmail.com>
2020-12-16 04:10:35 -0600

    Top Down (no memo)

---
+++ Dynamic_Programming/Pattern_3/Fib.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int fib(int n) {
+
+    if(n == 0) {
+        return 0;
+    }
+
+    if(n == 1) {
+        return 1;
+    }
+
+    return fib(n-1) + fib(n-2);
+
+}
+
+
+int main() {
+
+    cout << fib(7) << endl;
+    cout << fib(5) << endl;
+    cout << fib(6) << endl;
+
+    return 0;
+}
