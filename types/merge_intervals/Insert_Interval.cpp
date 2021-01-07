555363f41b634343f151fd529129bb63b3d8968e
Guru Sarath <gurusarath1.official@gmail.com>
2021-01-07 01:54:14 -0600

    Create Insert_Interval.cpp

---
+++ types/merge_intervals/Insert_Interval.cpp
@@ -0,0 +1,70 @@
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class Interval {
+ public:
+  int start = 0;
+  int end = 0;
+
+  Interval(int start, int end) {
+    this->start = start;
+    this->end = end;
+  }
+};
+
+class InsertInterval {
+ public:
+  static vector<Interval> insert(const vector<Interval> &intervals, Interval newInterval) {
+    vector<Interval> mergedIntervals;
+
+    int i = 0;
+
+    // insert all the intervals below the new Interval
+    while(i < intervals.size() && intervals[i].end < newInterval.start) {
+      mergedIntervals.push_back(intervals[i]);
+      i++;
+    }
+
+    // Chain and merge the new Interval
+    while(i < intervals.size() && intervals[i].start < newInterval.end) {
+      newInterval.start = min(intervals[i].start, newInterval.start);
+      newInterval.end = max(intervals[i].end, newInterval.end);
+      i++;
+    }
+
+    // Insert the newly merged interval
+    mergedIntervals.push_back(newInterval);
+
+    // Insert the rest of the remaining intervals
+    while(i < intervals.size()) {
+      mergedIntervals.push_back(intervals[i]);
+      i++;
+    }
+
+    return mergedIntervals;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  vector<Interval> input = {{1, 3}, {5, 7}, {8, 12}};
+  cout << "Intervals after inserting the new interval: ";
+  for (auto interval : InsertInterval::insert(input, {4, 6})) {
+    cout << "[" << interval.start << "," << interval.end << "] ";
+  }
+  cout << endl;
+
+  cout << "Intervals after inserting the new interval: ";
+  for (auto interval : InsertInterval::insert(input, {4, 10})) {
+    cout << "[" << interval.start << "," << interval.end << "] ";
+  }
+  cout << endl;
+
+  input = {{2, 3}, {5, 7}};
+  cout << "Intervals after inserting the new interval: ";
+  for (auto interval : InsertInterval::insert(input, {1, 4})) {
+    cout << "[" << interval.start << "," << interval.end << "] ";
+  }
+  cout << endl;
+}
