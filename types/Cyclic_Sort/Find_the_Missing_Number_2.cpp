92b722dc6510d852e529a54f3bc4aa4aa0c20bae
Guru Sarath <gurusarath1.official@gmail.com>
2021-01-10 01:25:23 -0600

    XOR method

---
+++ types/Cyclic_Sort/Find_the_Missing_Number_2.cpp
@@ -0,0 +1,27 @@
+/*
+We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’.
+Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
+*/
+
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class MissingNumber {
+ public:
+  static int findMissingNumber(vector<int> &nums) {
+    // Hasing Method
+    int missing_num = 0;
+
+    for(int num : nums) {
+      missing_num = missing_num ^ num;
+    }
+
+    for(int i=0; i<nums.size()+1; i++) {
+      missing_num = missing_num ^ i;
+    }
+
+    return missing_num;
+  }
+};
