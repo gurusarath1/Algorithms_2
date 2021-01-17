5b414055007c364fc497cc97bad5c5638c50c1b6
Guru Sarath <gurusarath1.official@gmail.com>
2021-01-17 13:13:58 -0600

    Create Subsets.cpp

---
+++ types/Sub_Sets/Subsets.cpp
@@ -0,0 +1,45 @@
+using namespace std;
+
+#include <iostream>
+#include <vector>
+
+class Subsets {
+ public:
+  static vector<vector<int>> findSubsets(const vector<int>& nums) {
+    vector<vector<int>> subsets;
+    vector<int> current_set;
+    support_findSubsets(nums, 0, current_set, subsets);
+    
+    return subsets;
+  }
+
+  static void support_findSubsets(const vector<int>& nums, int current_index, vector<int> &current_set, vector<vector<int>> &subsets) {
+
+    if(current_index > nums.size() - 1) {
+      subsets.push_back(current_set);
+      return;
+    }
+
+    support_findSubsets(nums, current_index+1, current_set, subsets);
+
+    current_set.push_back(nums[current_index]);
+
+    support_findSubsets(nums, current_index+1, current_set, subsets);
+
+    current_set.erase(current_set.end() - 1);
+
+    return;
+  }
+};
+
+int main(int argc, char* argv[]) {
+
+  auto result = Subsets::findSubsets(vector<int>{1,2,3,4,5,6});
+  cout << "Subsets are : " << endl;
+  for (auto vec : result) {
+    for (auto num : vec) {
+      cout << num << " ";
+    }
+    cout << endl;
+  }
+}
