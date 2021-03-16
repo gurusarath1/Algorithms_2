e4617ccd0607f3f86d09f93dbc4015d6670472d8
Guru Sarath <gurusarath1.official@gmail.com>
2021-03-16 09:03:49 -0500

    Update all_sub_arrays_with_target_sum.cpp

--- fundamental_units/all_sub_arrays_with_target_sum.cpp
+++ fundamental_units/all_sub_arrays_with_target_sum.cpp
@@ -4,24 +4,26 @@
 using namespace std;
 
 int main() {
-  vector<int> ary = { -1, 3, 5, 1, 4, 2, -9 };
-  int target = 6;
-  
-  int prefix_sum = 0;
-  int complement;
-  map<int, int> prefixSum_map;
+	vector<int> ary = { -1, 3, 5, 1, 4, 2, -9 };
+	int target = 6;
 
-  for (int i = 0; i < ary.size (); i++) {
-      prefix_sum += ary[i];
-      complement = prefix_sum - target;
-      prefixSum_map[prefix_sum] = i;
+	int prefix_sum = 0;
+	int complement;
+	map<int, int> prefixSum_map;
 
-      if (prefixSum_map.find(complement) != prefixSum_map.end()) {
-          // sub-array found
-          cout << "[ " << prefixSum_map[complement] + 1 << " , " << i << " ]"; // Print the index of subarray
-          
-      }
+	prefixSum_map[0] = -1; // if the subarray sum starts from index 0 // because we use "prefixSum_map[complement] + 1"
+
+	for (int i = 0; i < ary.size (); i++) {
+		prefix_sum += ary[i];
+		complement = prefix_sum - target;
+		prefixSum_map[prefix_sum] = i;
+
+		if (prefixSum_map.find(complement) != prefixSum_map.end()) {
+			// sub-array found
+			cout << "[ " << prefixSum_map[complement] + 1 << " , " << i << " ]"; // Print the index of subarray
+
+		}
 	}
-	
+
 	return 0;
 }
