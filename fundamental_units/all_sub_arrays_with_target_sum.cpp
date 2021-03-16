4562cfb104acb951b09e9a776aa1858af8fb7e28
Guru Sarath <gurusarath1.official@gmail.com>
2021-03-16 10:28:32 -0500

    Update all_sub_arrays_with_target_sum.cpp

--- fundamental_units/all_sub_arrays_with_target_sum.cpp
+++ fundamental_units/all_sub_arrays_with_target_sum.cpp
@@ -16,13 +16,13 @@ int main() {
 	for (int i = 0; i < ary.size (); i++) {
 		prefix_sum += ary[i];
 		complement = prefix_sum - target;
-		prefixSum_map[prefix_sum] = i;
 
 		if (prefixSum_map.find(complement) != prefixSum_map.end()) {
 			// sub-array found
 			cout << "[ " << prefixSum_map[complement] + 1 << " , " << i << " ]"; // Print the index of subarray
-
 		}
+		
+		prefixSum_map[prefix_sum] = i;
 	}
 
 	return 0;
