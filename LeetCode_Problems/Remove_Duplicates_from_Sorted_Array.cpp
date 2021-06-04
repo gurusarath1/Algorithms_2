3475c5405f0a3893807ada06bbfd453d5d553fdd
Guru Sarath <gurusarath1.official@gmail.com>
2021-06-04 11:29:28 -0500

    Update Remove_Duplicates_from_Sorted_Array.cpp

--- LeetCode_Problems/Remove_Duplicates_from_Sorted_Array.cpp
+++ LeetCode_Problems/Remove_Duplicates_from_Sorted_Array.cpp
@@ -40,3 +40,26 @@ public:
         return i;
     }
 };
+
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        if(nums.size() == 0) return 0;
+        
+        int j=1;
+        for(int i=1; i<nums.size(); i++) {
+            
+            if(nums[i-1] == nums[i]) {
+                continue;
+            }
+            
+            nums[j] = nums[i];
+            j++;   
+        }
+        
+        return j;
+        
+    }
+};
