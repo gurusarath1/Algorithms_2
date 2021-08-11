7fc00eea39845d98d85a9dcff3ee0e8e0e8f5d5e
Guru Sarath <gurusarath1.official@gmail.com>
2021-08-11 03:10:52 -0600

    Create Sum_of_Left_Leaves.cpp

---
+++ LeetCode_Problems/BinarySearchTree/Sum_of_Left_Leaves.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int sum = 0;
+    
+    int sumOfLeftLeaves(TreeNode* root) {
+        
+        if(root == nullptr) return sum;
+        
+        if(root->left != nullptr && root->left->left == nullptr && root->left->right == nullptr) {
+            sum += root->left->val;
+        } else {
+            sumOfLeftLeaves(root->left);
+        }
+        
+        sumOfLeftLeaves(root->right);
+        
+        return sum;
+        
+    }
+};
