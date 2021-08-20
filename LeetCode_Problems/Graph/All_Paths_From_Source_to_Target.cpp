4906ed950098bc44b0c539d82202795e0d93ee1f
Guru Sarath <gurusarath1.official@gmail.com>
2021-08-19 23:45:41 -0700

    Create All_Paths_From_Source_to_Target.cpp

---
+++ LeetCode_Problems/Graph/All_Paths_From_Source_to_Target.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    vector<vector<int>> result;
+    
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        
+        traverse(graph, 0, vector<int>{});
+        
+        return result;
+        
+    }
+    
+    void traverse(vector<vector<int>>& graph, int current_node, vector<int> path) {
+        
+        path.push_back(current_node);
+        
+        if(current_node == graph.size() - 1) result.push_back(path);
+        
+        for(int i=0; i<graph[current_node].size(); i++) {
+            traverse(graph, graph[current_node][i], path);
+        }
+        
+    }
+    
+};
