f0d48f01a71f18609681f6bbcdbb69beef699d1e
Guru Sarath <gurusarath1.official@gmail.com>
2021-08-21 22:57:27 -0700

    Create Minimum_Height_Trees.cpp

---
+++ LeetCode_Problems/Graph/Minimum_Height_Trees.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        
+
+        vector<int> x;
+        x.reserve(n);
+        vector<vector<int>> graph(n, x);
+        
+        for(vector<int> edge : edges) {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        
+        int height;
+        int min_height = INT_MAX;
+        int heights_map[n];
+        for(int i=0; i<n; i++) {
+            height = traverse(graph, i, -1);
+            min_height = min(min_height, height);
+            heights_map[i] = height;
+        }
+        
+        vector<int> result;
+        result.reserve(n);
+        for(int i=0; i<n; i++) {
+            if(heights_map[i] == min_height) {
+                result.push_back(i);
+            }
+        } 
+        
+        return result;
+        
+    }
+    
+    int traverse(vector<vector<int>> &graph, int current_node, int parent) {
+        
+        int path_len = 0;
+        int max_path_len = 0;
+        for(int i=0; i<graph[current_node].size(); i++) {
+            if(graph[current_node][i] != parent) {
+                path_len =  traverse(graph, graph[current_node][i], current_node);
+                max_path_len = max(max_path_len, path_len);
+            }
+        }
+        
+        return max_path_len + 1;
+    }
+    
+    
+};
