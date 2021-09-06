20aa3b5ea3155779a598e4cd7bc95359054c552f
Guru Sarath <gurusarath1.official@gmail.com>
2021-09-05 21:31:32 -0700

    Update Set_Clear_Toggle_Bit.cpp

--- fundamental_units/Set_Clear_Toggle_Bit.cpp
+++ fundamental_units/Set_Clear_Toggle_Bit.cpp
@@ -5,21 +5,21 @@ using namespace std;
 int main()
 {
 
-    // Setting 
+    // Setting // Use OR
     unsigned char x = 4;
     // set 7th bit  7 6 5 4 3 2 1 0
     x = x | (1 << 7);
     
     cout << (int)x << endl;
     
-    // Clearing
+    // Clearing // Use AND
     x = 127;
     // Clear 4th bit
     x = x & ~(1 << 4);
     
     cout << (int)x << endl;
     
-    // Toggling
+    // Toggling // Use XOR
     x = 30;
     // Toggle 5th bit
     x = x ^ (1 << 5);
