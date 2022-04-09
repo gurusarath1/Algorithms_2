173d844c43cdfcdb7deafe3f3a72ec19a90dec17
Guru Sarath <gurusarath1.official@gmail.com>
2022-04-08 18:34:18 -0700

    Update Top_K_Frequent_Elements.cpp

--- LeetCode_Problems/heap/Top_K_Frequent_Elements.cpp
+++ LeetCode_Problems/heap/Top_K_Frequent_Elements.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-        class Compare
-    {
-    public:
-        bool operator() (pair<double,int> a, pair<double,int> b)
-        {
-            return a.second < b.second;
-        }
-    };
     
+    static bool compare(pair<int,int> a, pair<int,int> b) {
+        return a.second < b.second;
+    }
     
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
-        map<int, int> freq;
+       map<int, int> freq;
         
         for(int num : nums) {
             freq[num] += 1;
         }
         
-        priority_queue<pair<int,int>, vector<pair<int,int>>, Compare> pq;
+        priority_queue< pair<int,int> , vector<pair<int,int>>, function<bool(pair<int,int>, pair<int,int>)> > pq(compare);
         
         for(pair<int,int> p : freq) {
             pq.push(p);
         }
         
         vector<int> result;
+        
         for(int i=0; i<k; i++) {
             result.push_back(pq.top().first);
             pq.pop();
         }
         
         return result;
+        
     }
 };
