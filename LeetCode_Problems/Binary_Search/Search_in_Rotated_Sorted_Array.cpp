9d4df0a5d7bbaf420ebf4b87bcb1125507092f15
Guru Sarath <gurusarath1.official@gmail.com>
2024-07-26 14:53:20 -0400

    Update Search_in_Rotated_Sorted_Array.cpp

--- LeetCode_Problems/Binary_Search/Search_in_Rotated_Sorted_Array.cpp
+++ LeetCode_Problems/Binary_Search/Search_in_Rotated_Sorted_Array.cpp
@@ -1,54 +1,33 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        
-        if(nums.size() == 1 && nums[0] != target) return -1;
-        if(nums.size() == 1 && nums[0] == target) return 0;
-        
-      
-        /* FIND THE PIVOT */
-        int left = 0;
-        int right = nums.size() - 1;
-        int mid = left + (right - left) / 2;;
-        
-        while(left < right) {
-            if(nums[mid] > nums[right]) { // This is not normal in a sorted array
-                left = mid + 1;
-            } else { // This is normal
-                right = mid;
-            }
-            mid = left + (right - left) / 2;
-        }
-        
-        int pivot_index = mid; // This is also the smallest element
-        
-      
-      
-      /* SEARCH LEFT OR RIGHT */
-        if(target > nums[nums.size() - 1]) {
-            // search left half
-            left = 0;
-            right = pivot_index - 1;
-        } else {
-            // Search right half
-            left = pivot_index;
-            right = nums.size() - 1;
-        }
-        
-        
-        while(left <= right) {
-            mid = left + (right - left) / 2;
-            
-            if(nums[mid] == target) {
+
+        int low = 0;
+        int high = nums.size() - 1;
+
+        while(low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if(target == nums[mid]) {
                 return mid;
-            } else if(nums[mid] < target) {
-                left = mid + 1;
+            } else if(nums[mid] >= nums[low]) {
+                if(target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             } else {
-                right = mid - 1;
+                if(target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
+
         }
         
         return -1;
-        
     }
 };
+
