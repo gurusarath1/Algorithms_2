1d053e6f3a291024a1f41e3c56b97f578bba1511
Guru Sarath <gurusarath1.official@gmail.com>
2024-08-11 02:05:38 -0400

    Update Find_K_Closest_Elements.cpp

--- LeetCode_Problems/Binary_Search/Find_K_Closest_Elements.cpp
+++ LeetCode_Problems/Binary_Search/Find_K_Closest_Elements.cpp
@@ -1,61 +1,92 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int left = 0;
-        int right = arr.size() - 1;
-        int mid;
-        
-        while(left <= right) {
-            
-            mid = left + (right - left) / 2;
-            
+
+        // Step 1: Find the first closest element to x
+
+        int low = 0;
+        int high = arr.size() - 1;
+        int first_closest_num_index = -1;
+        int sub_arry_start_index = -1;
+        int sub_arry_end_index = -1;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
             if(arr[mid] == x) {
+                first_closest_num_index = mid; // If x is found, then mid index is the first closest index
                 break;
-            }
-            
-            if(arr[mid] > x) {
-                right = mid - 1;
+            } else if(arr[mid] > x) {
+                high = mid - 1;
             } else {
-                left = mid + 1;
+                low = mid + 1;
             }
         }
-        
-        vector<int> result;
-        //result.push_back(arr[mid]);
-        int i = mid-1;
-        int j = mid;
-        
-        if(i < 0) {
-            i = mid;
-            j = mid + 1;
-        }
-        
-        for(int count = 0; count < k; count ++) {
+
+        // If x is not in the array, index high or low will have the first closest number
+        if(first_closest_num_index == -1) {
             
-            int left_dist = INT_MAX;
-            int right_dist = INT_MAX;
-            
-            if(i >= 0) { left_dist = abs(arr[i] - x); }
-            if(j < arr.size()) { right_dist = abs(arr[j] - x); }
-        
-            if(left_dist > right_dist) {
-                result.push_back(arr[j]);
-                j++;
-            } else if (left_dist < right_dist) {
-                result.push_back(arr[i]);
-                i--;
+            if(high == -1) { // x less than all elements in the array
+                first_closest_num_index = low;
+            } else if (low == arr.size()) { // x greater than all elements in the array
+                first_closest_num_index = high;
+            } else if( abs(arr[high] - x) > abs(arr[low] - x) ) { // high or low index is the closest
+                first_closest_num_index = low;
             } else {
-                
-
-                    result.push_back(arr[i]);
-                    i--;                    
-
+                first_closest_num_index = high;
             }
-            
         }
+
+        vector<int> ret;
+
+        if(high == -1) { // first k elements
+            sub_arry_start_index = first_closest_num_index;
+            sub_arry_end_index = k - 1;
+        } else if(low == arr.size()) { // last k elements
+            sub_arry_start_index = first_closest_num_index - k + 1;
+            sub_arry_end_index = first_closest_num_index;
+        } else {
+
+            // Step 2: Two pointer algo to find the range
+
+            int ptr_1 = first_closest_num_index - 1;
+            int ptr_2 = first_closest_num_index + 1;
+            int num_values_collected = 1; // 1 value already collected (first closest number)
+
+            while(num_values_collected < k) {
+                if( abs(arr[ptr_1] - x) > abs(arr[ptr_2] - x) ) {
+                    ptr_2++;
+                    num_values_collected++;
+
+                    // reached end of array ?
+                    if(ptr_2 == arr.size()) {
+                        ptr_1 -= (k - num_values_collected);
+                        num_values_collected = k;
+                        break;
+                    }
+                } else {
+                    ptr_1--;
+                    num_values_collected++;
+
+                    // reached the beginning of array ?
+                    if(ptr_1 == -1) {
+                        ptr_2 += (k - num_values_collected);
+                        num_values_collected = k;
+                        break;
+                    }
+                }
+            }
+
+            // Start and end indices
+            sub_arry_start_index = ptr_1 + 1;
+            sub_arry_end_index = ptr_2 - 1;
+        }
+
+        // Create the subarray;
+        for(int i=sub_arry_start_index; i<=sub_arry_end_index; i++) {
+            ret.push_back(arr[i]);
+        }
+        return ret;
         
-        sort(result.begin(), result.end());
-        
-        return result;
     }
 };
