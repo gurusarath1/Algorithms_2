0293165d4e73fd644a210243efaec32e02d7a9a3
Guru Sarath <gurusarath1.official@gmail.com>
2024-08-21 23:47:51 -0400

    Update Maximum_Profit_in_Job_Scheduling.cpp

--- LeetCode_Problems/DP_memo/Maximum_Profit_in_Job_Scheduling.cpp
+++ LeetCode_Problems/DP_memo/Maximum_Profit_in_Job_Scheduling.cpp
@@ -19,35 +19,57 @@ public:
         return max_profit(jobs, 0, 0, memo);
     }
 
+    int bin_search(vector<vector<int>> &jobs, int end_time, int low, int high) {
+
+        int last_index = high;
+
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(jobs[mid][0] >= end_time) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        if(low == last_index+1) return -1;
+
+        return low;
+    }
+
     int max_profit(vector<vector<int>> &jobs, int current_index, int prev_end_time, map<int,int> &memo) {
 
         if(current_index >= jobs.size()) return 0;
+
+        if(memo.find(current_index) != memo.end()) {
+            return memo[current_index];
+        }
 
         int current_start_time = jobs[current_index][0];
         int profit_if_job_included = 0;
         if(prev_end_time <= current_start_time) {
             // Can include the job
-
-            if(memo.find(current_index) != memo.end()) {
-                //cout << "Memo Hit";
-                return memo[current_index];
-            }
-
-
             int profit_from_this_job = jobs[current_index][2];
             int current_end_time = jobs[current_index][1];
-            int next_job_sub_tree_profit = max_profit(jobs, current_index+1, current_end_time, memo);
-            profit_if_job_included = next_job_sub_tree_profit + profit_from_this_job;
+
+            int next_doable_job_index = bin_search(jobs, current_end_time, current_index+1, jobs.size()-1);
+
+            if(next_doable_job_index == -1) {
+                profit_if_job_included = profit_from_this_job;
+            } else {
+                int next_job_sub_tree_profit = max_profit(jobs, next_doable_job_index, current_end_time, memo);
+                profit_if_job_included = next_job_sub_tree_profit + profit_from_this_job;
+            }
             
         }
 
         // Current Job not included
         int profit_if_job_not_included = max_profit(jobs, current_index+1, prev_end_time, memo);
 
-        if(prev_end_time <= current_start_time) {
-            memo[current_index] = max(profit_if_job_included, profit_if_job_not_included);
-        }
+        // Memo
+        memo[current_index] = max(profit_if_job_included, profit_if_job_not_included);
 
-        return max(profit_if_job_included, profit_if_job_not_included);
+        return memo[current_index];
     }
 };
