4613f3d4e58c7d7cba1b96f917f7df4126883da4
Guru Sarath <gurusarath1.official@gmail.com>
2024-10-07 15:39:38 -0400

    Create Minimum_String_Length_After_Removing_Substrings.cpp

---
+++ LeetCode_Problems/stack/Minimum_String_Length_After_Removing_Substrings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minLength(string s) {
+
+        stack<char> stk;
+
+        for(char c : s) {
+            if(!stk.empty() && ((stk.top() == 'A' && c == 'B') || (stk.top() == 'C' && c == 'D')) ) {
+                stk.pop();
+                continue;
+            } else {
+                stk.push(c);
+            }
+        }
+
+        return stk.size();
+        
+    }
+};
