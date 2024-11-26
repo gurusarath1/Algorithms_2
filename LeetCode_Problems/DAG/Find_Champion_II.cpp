b145f6fde3e9bc6ebe46c7afe124026d7225dd82
Guru Sarath <gurusarath1.official@gmail.com>
2024-11-25 19:40:39 -0500

    Update Find_Champion_II.cpp

--- LeetCode_Problems/DAG/Find_Champion_II.cpp
+++ LeetCode_Problems/DAG/Find_Champion_II.cpp
@@ -3,20 +3,22 @@ public:
     int findChampion(int n, vector<vector<int>>& edges) {
         unordered_set<int> result_nodes;
 
-        for(int i=0; i<n; i++) {
-            result_nodes.insert(i);
-        }
+        if(edges.size() >= n-1) {
+            for(int i=0; i<n; i++) {
+                result_nodes.insert(i);
+            }
 
-        for(vector<int> &edge : edges) {
-            result_nodes.erase(edge[1]);
-        }
+            for(vector<int> &edge : edges) {
+                result_nodes.erase(edge[1]);
+            }
 
-        if(result_nodes.size() == 1) {
-            return *result_nodes.begin();
-        }
+            if(result_nodes.size() == 1) {
+                return *result_nodes.begin();
+            }
 
-        if(result_nodes.size() == 0 && n == 1) {
-            return 0;
+            if(result_nodes.size() == 0 && n == 1) {
+                return 0;
+            }
         }
 
         return -1;
