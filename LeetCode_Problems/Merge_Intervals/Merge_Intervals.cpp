6b0572d84a03c1743458345c1f0d8b8350df0c63
Guru Sarath <gurusarath1.official@gmail.com>
2026-08-09 12:08:25 -0400

    Update Merge_Intervals.cpp

--- LeetCode_Problems/Merge_Intervals/Merge_Intervals.cpp
+++ LeetCode_Problems/Merge_Intervals/Merge_Intervals.cpp
@@ -1,32 +1,48 @@
+/*
+Given an array of intervals where intervals[i] = [starti, endi],
+merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+*/
+
 class Solution {
 public:
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        
-        sort(intervals.begin(), intervals.end(), intervalCompare);
-        
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        vector<vector<int>> result;
-        
-        for(int i=1; i<intervals.size(); i++) {
-            
-            if(intervals[i][0] <= end) {
-                
-                if(end < intervals[i][1]) end = intervals[i][1];
-            } else {
-                
-                result.push_back( vector<int>{start, end} );
-                start = intervals[i][0];
-                end = intervals[i][1];
-            }
-            
-        }
-        result.push_back( vector<int>{start, end} );
-        return result;
-        
+
+    static bool compareStartTimes(vector<int> &a, vector<int> &b) {
+        return a[0] < b[0];
     }
-    
-    static bool intervalCompare(vector<int> x, vector<int> y) {
-        return x[0] < y[0];
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        vector<vector<int>> merged;
+
+        // Start based on start times
+        sort(intervals.begin(), intervals.end(), compareStartTimes);
+
+        // Current window
+        int curr_start = intervals[0][0];
+        int curr_end = intervals[0][1];
+
+        for(int i=1; i<intervals.size(); i++) {
+            int start_i = intervals[i][0];
+            int end_i = intervals[i][1];
+
+            if(start_i > curr_end) {
+                // No Overlap case = Start/Create new interval
+                merged.push_back(vector<int>{curr_start, curr_end});
+                curr_start = start_i;
+                curr_end = end_i;
+            } else {
+                // Overlap case
+                if(end_i > curr_end) { // If end time exceeds
+                    curr_end = end_i; // Update end time
+                } else {
+                    ; // Interval i is fully inside the current running interval
+                }
+            }
+        }
+
+        // Push the last running interval
+        merged.push_back(vector<int>{curr_start, curr_end});
+
+        return merged;
     }
 };
