4d0f6773fdc41e73c33f27413f54530386f4011e
Guru Sarath <gurusarath1.official@gmail.com>
2026-08-09 16:51:22 -0400

    Refactor binary search logic and variable names
    Refactor binary search implementation to use different variable names and update loop conditions.

--- LeetCode_Problems/Binary_Search/Binary_Search.cpp
+++ LeetCode_Problems/Binary_Search/Binary_Search.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+
+    // [lo, hi] -----
     int search(vector<int>& nums, int target) {
         
         int start_index = 0;
@@ -20,6 +22,36 @@ public:
             
         }
         
+        return -1;
+        
+    }
+};
+
+
+
+class Solution {
+public:
+
+    // [lo, hi) -----
+    int search(vector<int>& nums, int target) {
+
+        int l = 0, h = nums.size(); // <---- diff
+
+        while(l < h) { // <---- diff
+            
+            int m = l + (h - l) / 2;
+
+            if(nums[m] < target) {
+                l = m + 1;
+                
+            } else if (nums[m] > target){
+                h = m; // <---- diff
+                
+            } else {
+                return m;
+            }
+        }
+
         return -1;
         
     }
