5dec33b42ceb856c18688b8850ba5482c07c8378
Guru Sarath <gurusarath1.official@gmail.com>
2026-08-10 22:00:38 -0400

    Refactor findMin to handle duplicates in array
    Refactor findMin function to handle duplicates and improve logic for finding the minimum element in a rotated sorted array.

--- LeetCode_Problems/Binary_Search/Find_Minimum_in_Rotated_Sorted_Array_II.cpp
+++ LeetCode_Problems/Binary_Search/Find_Minimum_in_Rotated_Sorted_Array_II.cpp
@@ -1,28 +1,44 @@
+/*
+Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,4,4,5,6,7] might become:
+
+[4,5,6,7,0,1,4] if it was rotated 4 times.
+[0,1,4,4,5,6,7] if it was rotated 7 times.
+Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+Given the sorted rotated array nums that may contain duplicates, return the minimum element of this array.
+
+You must decrease the overall operation steps as much as possible.
+*/
+
 class Solution {
 public:
     int findMin(vector<int>& nums) {
+        int l = 0;
+        int h = nums.size() - 1;
+        int p = -1;
 
-        int low = 0;
-        int high = nums.size() - 1;
+        // MAIN IDEA: Pivot might be in the sorted half
+        // Keep updating the pivot in the sorted half
 
-        while(low < high) {
-            int mid = low + (high - low) / 2;
+        while(l <= h) {
+            int m = l + (h - l) / 2;
 
-            if(nums[mid] == nums[low] && nums[mid] == nums[high]) {
-                // Can't decide to go left or right
-                // But we know for sure nums[high] is not the pivot
-                high -= 1;
-            } else if(nums[mid] <= nums[high]) {
-                // Right is sorted (you can't do similar thing if left is sorted because pivot can be left)
-                // Go left
-                high = mid;
-            } else {
-                // Right is not sorted. Left may or may not be sorted
-                // Go Right
-                low = mid + 1; // mid is not the pivot
+            if(nums[l] == nums[m] && nums[m] == nums[h]) {
+                if(p==-1 || (nums[l] < nums[p])) p = l;
+                l++;
+                h--;
+                continue;
+            }
+
+            if(nums[l] <= nums[m]) { // Left side sorted
+                if(p==-1 || (nums[l] < nums[p])) p = l;
+                l = m + 1;
+            } else { // Right side sorted
+                if(p==-1 || (nums[m] < nums[p])) p = m;
+                h = m - 1;
             }
         }
         
-        return nums[low];
+        return nums[p];
     }
 };
