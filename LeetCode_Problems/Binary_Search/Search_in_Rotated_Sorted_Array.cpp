3b3c2f569db2bd720d20ee8b31cad114a3823aad
Guru Sarath <gurusarath1.official@gmail.com>
2026-08-10 20:32:41 -0400

    Update Search_in_Rotated_Sorted_Array.cpp

--- LeetCode_Problems/Binary_Search/Search_in_Rotated_Sorted_Array.cpp
+++ LeetCode_Problems/Binary_Search/Search_in_Rotated_Sorted_Array.cpp
@@ -1,33 +1,49 @@
+/*
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length)
+such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed).
+For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+*/
+
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
 
-        int low = 0;
-        int high = nums.size() - 1;
+        int l=0;
+        int h = nums.size() - 1;
 
-        while(low <= high) {
+        while(l <= h) {
+            int m = l + (h - l) / 2;
 
-            int mid = low + (high - low) / 2;
+            if(nums[m] == target) return m;
 
-            if(target == nums[mid]) {
-                return mid;
-            } else if(nums[mid] >= nums[low]) {
-                if(target >= nums[low] && target < nums[mid]) {
-                    high = mid - 1;
+            // In <= the equal (=) is important because l might be equal m.
+            if(nums[l] <= nums[m]) { // left side is sorted
+
+                if(nums[l] <= target && target < nums[m]) { // target on left side
+                    h = m - 1;
                 } else {
-                    low = mid + 1;
+                    l = m + 1;
                 }
-            } else {
-                if(target > nums[mid] && target <= nums[high]) {
-                    low = mid + 1;
+
+            } else { // right side is sorted
+
+                if(nums[m] < target && target <= nums[h]) { // target on right side
+                    l = m + 1;
                 } else {
-                    high = mid - 1;
+                    h = m - 1;
                 }
+
             }
-
         }
-        
+
         return -1;
     }
 };
-
