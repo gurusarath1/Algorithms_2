faa2ef06f185faf8e70d167b6ca766203ce4b944
Guru Sarath <gurusarath1.official@gmail.com>
2026-09-09 14:04:45 -0400

    Refactor intersection detection logic in linked lists
    Refactor getIntersectionNode method to simplify logic and improve readability.

--- LeetCode_Problems/LinkedList/Intersection_of_Two_Linked_Lists2.cpp
+++ LeetCode_Problems/LinkedList/Intersection_of_Two_Linked_Lists2.cpp
@@ -1,3 +1,25 @@
+/*
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+
+For example, the following two linked lists begin to intersect at node c1:
+
+
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+
+Note that the linked lists must retain their original structure after the function returns.
+
+Custom Judge:
+
+The inputs to the judge are given as follows (your program is not given these inputs):
+
+intersectVal - The value of the node where the intersection occurs. This is 0 if there is no intersected node.
+listA - The first linked list.
+listB - The second linked list.
+skipA - The number of nodes to skip ahead in listA (starting from the head) to get to the intersected node.
+skipB - The number of nodes to skip ahead in listB (starting from the head) to get to the intersected node.
+The judge will then create the linked structure based on these inputs and pass the two heads, headA and headB to your program. If you correctly return the intersected node, then your solution will be accepted.
+*/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -9,37 +31,24 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        
-        ListNode* node1 = headA;
-        ListNode* node2 = headB;
-        
-        bool switched1 = false;
-        bool switched2 = false;
-        
-        while(node1 != node2) {
-            
-            node1 = node1->next;
-            node2 = node2->next;
-            
-            if(node1 == nullptr) {
-                
-                if(switched1) return nullptr;
-                
-                switched1 = true;
-                node1 = headB;
+
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        while(a != b) {
+            if(a == nullptr) {
+                a = headB;
+            } else {
+                a = a->next;
             }
-            
-            if(node2 == nullptr) {
-                
-                if(switched2) return nullptr;
-                
-                switched2 = true;
-                node2 = headA;
+
+            if(b == nullptr) {
+                b = headA;
+            } else {
+                b = b->next;
             }
-            
         }
-        
-        return node1;
-        
+
+        return a; // a can be nullptr if they don't intersect
     }
 };
