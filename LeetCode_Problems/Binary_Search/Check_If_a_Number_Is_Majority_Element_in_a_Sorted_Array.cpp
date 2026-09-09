06522e125936054326a5283355d7cc3eced14620
Guru Sarath <gurusarath1.official@gmail.com>
2026-09-09 01:07:48 -0400

    Add function to check majority element in sorted array

---
+++ LeetCode_Problems/Binary_Search/Check_If_a_Number_Is_Majority_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isMajorityElement(vector<int>& nums, int target) {
+
+        int first_occ = -1;
+        int last_occ = -1;
+        int thres = nums.size() / 2;
+
+        int l=0;
+        int h=nums.size() - 1;
+
+        while(l <= h) {
+            int m = l + (h - l) / 2;
+
+            if(nums[m] == target) {
+                first_occ = m;
+                h = m - 1;
+            } else if(nums[m] < target) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+
+        if(first_occ == -1) return false;
+
+        l = 0;
+        h = nums.size() - 1;
+        while(l <= h) {
+            int m = l + (h - l) / 2;
+
+            if(nums[m] == target) {
+                last_occ = m;
+                l = m + 1;
+            } else if(nums[m] < target) {
+                l = m + 1;
+            } else {
+                h = m - 1;
+            }
+        }
+
+        int num_occ = last_occ - first_occ + 1;
+
+        if(num_occ > thres) {
+            return true;
+        }
+        
+        return false;
+    }
+};
